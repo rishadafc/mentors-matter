014abe9572daac2dfc28624a0fdb12500b85f4a4
Rishad Bharucha <rb2102@gmail.com>
2014-04-05 16:33:24 -0600

    Add Pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 MentorsMatter::Application.routes.draw do
+  get "pages/home"
+  get "pages/about"
   devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
