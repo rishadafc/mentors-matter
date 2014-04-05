2af1ac5e0e372e3ac917420d7bd734e46daceffe
Rishad Bharucha <rb2102@gmail.com>
2014-04-05 14:59:05 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+MentorsMatter::Application.config.secret_key_base = '7a5ff1603e1eb462989bccb00cdc55c2f4397066c6d9ce86a67c4a41b815b784240a83fd6ff4a7c0e6111585f9cef5d01d8a0ddbaff75a2b46c023b9dd4629d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+MentorsMatter::Application.config.session_store :cookie_store, key: '_mentors-matter_session'
