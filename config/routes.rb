a9c01c58d9a2e194a4fe64eeb15cf53e9bb9bfa1
Rishad Bharucha <rb2102@gmail.com>
2014-04-05 16:37:48 -0600

    Update routes for home and about pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,14 @@
 MentorsMatter::Application.routes.draw do
-  get "pages/home"
-  get "pages/about"
+  root 'pages#home'
+
+  get "about" => "pages#about"
+
   devise_for :users
+  
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
